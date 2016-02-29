acce86c60490ee9fd87c0d89eee457b249732e77
Elise Tellier <elise.tellier@insa-rouen.fr>
2016-02-29 19:50:53 +0100

    Segmentation des images avec l'algo des kmeans et le choix des composantes en param

--- seance2/segmentation_kmeans.m
+++ seance2/segmentation_kmeans.m
@@ -4,12 +4,19 @@ function [segmentation, coeff_dice, taux_err] = segmentation_kmeans(fichier_imag
 % Calcule le taux d'erreur obtenue par cette m�thode de segmentation, la
 % matrice de Confusion, ainsi que le coefficient de Dice.
 
+% ENTREES :
 % fichier_image : le nom du fichier de l'image � tester
 % fichier_segmManuelle : le nom du fichier contenant l'image de
 % segmentation manuelle (v�rit� terrain)
 % Espace Couleur = {RGB, LAB, AB}
 % affichage : pr�ciser cette valeur � 1 si un affichage des images
 % r�sultats est souhait�.
+
+% SORTIES :
+% segmentation : image BINAIRE (0-1) de la segmentation obtenue; Utile pour
+% le post-Traitement
+% coeff_dice : le coefficient de dice obtenu
+% taux_err : le tax d'erreur issu de la mat de confusion
 
     if nargin < 4
         affichage=0;
@@ -26,6 +33,8 @@ function [segmentation, coeff_dice, taux_err] = segmentation_kmeans(fichier_imag
         X = redimLAB(image, 3);
     elseif strcmp(espaceCouleur, 'RGB')
         X = redimRGB(image);
+    elseif strcmp(espaceCouleur, 'HSV')
+        X = redimHSV(image);
     else
         fprintf('Erreur : Espace couleur inconnu (RGB, LAB ou AB)');
     end
@@ -34,20 +43,20 @@ function [segmentation, coeff_dice, taux_err] = segmentation_kmeans(fichier_imag
 
     % Kmeans sur l'image X :
     nbClusters=2;
-    [ypred, cluster_center] = kmeans(X, nbClusters, 'distance','sqEuclidean');
-    
+    [ypred, cluster_center] = kmeans(X, nbClusters, 'distance','sqEuclidean', 'emptyaction','singleton');
     % Calcul des erreurs, dice et matConf
     [segmentation, coeff_dice, taux_err] = statistiques(fichier_segmManuelle, ypred);
        
     % Affichage si demand� :
     if (affichage==1)
-        figure (1)
+        figure
         subplot(1, 3, 1)
-        imagesc(image)
+        imshow(image), title('Photo originale');
         subplot(1, 3, 2)
-        imagesc(segmManuelle)
+        segmManuelle = imread(fichier_segmManuelle);
+        imshow(segmManuelle), title('Vérité terrain')
         subplot(1, 3, 3)
-        imagesc(segmentation)
+        imshow(segmentation), title('Segmentation Kmeans')
     end
     
     
